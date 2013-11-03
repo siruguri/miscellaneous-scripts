8aa9210e8d034cfbc83e21c752d37bfadfc05675
Sameer Siruguri <siruguri@gmail.com>
2013-11-03 14:38:48 -0800

    refactoring markdown; code broken

--- markdown_extended.rb
+++ markdown_extended.rb
@@ -11,16 +11,6 @@ opts = GetoptLong.new(
 
 output_dir=nil
 
-opts.each do |opt, arg|
-  case opt
-  when '--help'
-    CommonUtils.print_help_and_exit
-    
-  when '--output_directory'
-    output_dir = arg.to_s
-  end
-end
-
 class RenderWithoutCode < Redcarpet::Render::HTML
 
   # Code will be in a separately styled div, with the class name 'mycode'
@@ -50,37 +40,65 @@ end
 
 input = '<link href="markdown.css" rel="stylesheet"></link>' + "\n"
 
-begin
-  input += File.open(ARGV[0]).readlines.join("")
-rescue Errno::ENOENT => e
-  CommonUtils.error_exit("#{ARGV[0]} Markdown file not found.")
+class Processor
+  def initialize(opts)
+    @output_dir=opts[:output_dir]
+    if !Dir.exists? @output_dir
+      CommonUtils.error_exit("#{output_dir}: specified directory doesn't exist.")
+    end
+  end
+
+  def process_file(filename)
+    begin
+      input += File.open(filename).readlines.join("")
+    rescue Errno::ENOENT => e
+      CommonUtils.error("#{filename} Markdown file not found.")
+      return nil
+    end
+    
+    outname=filename.gsub("\.md", ".html")
+    outname = File.join(@output_dir, outname)
+    puts outname
+
+    begin
+      f=File.open(outname, 'w')
+    rescue Errno::ENOENT => e
+      CommonUtils::error_exit("Cannot open output file #{outname} - fatal.")
+    end 
+
+    parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC)
+    toc = parser.render input
+
+    # Print the TOC
+    puts toc
+    f.puts toc
+
+    # Change the quotes smartly
+    quoted_out = Redcarpet::Render::SmartyPants.render input
+
+    # Do the rest of the rendering
+    renderer = RenderWithoutCode.new(with_toc_data: true)
+    parser = Redcarpet::Markdown.new(renderer)
+    #parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :footnotes => true)
+
+    f.puts (parser.render quoted_out)
+  end
 end
 
-outname=ARGV[0].gsub("\.md", ".html")
-outname = File.join(output_dir, outname) unless output_dir.nil?
+opts.each do |opt, arg|
+  case opt
+  when '--help'
+    CommonUtils.print_help_and_exit
+    
+  when '--output_directory'
+    output_dir = arg.to_s
+  end
+end
 
-puts outname
-begin
-  f=File.open(outname, 'w')
-rescue Errno::ENOENT => e
-  msg_part2 = ((Dir.exists?output_dir)?"":'(Maybe you did not create the directory you specified via -o?)')
-  CommonUtils::error_exit("Cannot open output file #{msg_part2}")
-end 
+if output_dir.nil?
+  CommonUtils.error_exit("Need -o/--output_directory argument. Exiting.")
+end
 
-parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC)
-toc = parser.render input
-
-# Print the TOC
-puts toc
-f.puts toc
-
-# Change the quotes smartly
-quoted_out = Redcarpet::Render::SmartyPants.render input
-
-# Do the rest of the rendering
-renderer = RenderWithoutCode.new(with_toc_data: true)
-parser = Redcarpet::Markdown.new(renderer)
-#parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :footnotes => true)
-
-f.puts (parser.render quoted_out)
+my_proc=Processor.new(output_dir: output_dir)
+puts ARGV
 
