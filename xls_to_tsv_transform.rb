f6df6ac87a13d5f2c6fb6d6f8fa949dfc41149ce
Sameer Siruguri <siruguri@gmail.com>
2014-12-24 16:27:07 -0800

    improved rubyxl script

--- xls_to_tsv_transform.rb
+++ xls_to_tsv_transform.rb
@@ -2,11 +2,14 @@ require 'rubyXL'
 # became necessary because XLS on OSX does stupid stuff to utf 8 encoding
 
 workbook = RubyXL::Parser.parse(ARGV[0])
-sheet = workbook.worksheets[0] # Returns first worksheet
+data = workbook[0].extract_data # Returns first worksheet
 
-sheet.sheet_data.each do |row|
-  row.each do |cell|
-    puts "#{cell}\t"
+data.each do |row|
+  if row
+    row.each do |cell|
+      print "#{cell}\t"
+    end
+    puts
   end
 end
 
